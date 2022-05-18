0d110411281e2e7bf2829d8eecb29372068f84a0
Vaishnavi Dayare <vaishnavidayare45@hotmail.com>
2022-05-17 21:20:51 -0500

    clock implementation

--- Mario/Application.cpp
+++ Mario/Application.cpp
@@ -9,10 +9,26 @@ int main()
 {
     sf::RenderWindow window(sf::VideoMode(1366, 768), "SFML works!");
     window.clear(sf::Color(41, 41, 41));
-    sf::CircleShape circle;
-    circle.setRadius(60.f);
-    circle.setFillColor(sf::Color(32,32,32));
-    circle.setPosition(600,300);
+    sf::CircleShape incircle;
+    incircle.setRadius(80.f);
+    incircle.setFillColor(sf::Color(32,32,32));
+    incircle.setPosition(570,270);
+    
+    sf::CircleShape midcircle;
+    midcircle.setRadius(150.f);
+    midcircle.setFillColor(sf::Color(145,149,203));
+    midcircle.setPosition(500,200);
+
+    sf::CircleShape outcircle;
+    outcircle.setRadius(200.f);
+    outcircle.setFillColor(sf::Color(120,192,240));
+    outcircle.setPosition(450,150);
+
+    sf::CircleShape outmcircle;
+    outmcircle.setRadius(250.f);
+    outmcircle.setFillColor(sf::Color(136,148,227));
+    outmcircle.setPosition(400,100);
+   
 
     while (window.isOpen())
     {
@@ -24,7 +40,10 @@ int main()
         }
 
         window.clear(sf::Color(41, 41, 41));
-        window.draw(circle);
+        window.draw(outmcircle);
+        window.draw(outcircle);
+        window.draw(midcircle);
+        window.draw(incircle);
         window.display();
     }
 
